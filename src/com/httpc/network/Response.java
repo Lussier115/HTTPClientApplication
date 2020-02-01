301f9307cd3026299e043b475ac2b6bdabeecb43
William-Andrew Lussier <w_lussie@encs.concordia.ca>
2020-02-01 14:35:32 -0500

    Getter and Setter

--- src/com/httpc/network/Response.java
+++ src/com/httpc/network/Response.java
@@ -9,4 +9,45 @@ public class Response {
     private int phrase;
     private RequestHeader header;
 
+    public Response() {
+    }
+
+    public Response(String version, int statusCode, int phrase, RequestHeader header) {
+        this.version = version;
+        this.statusCode = statusCode;
+        this.phrase = phrase;
+        this.header = header;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public int getPhrase() {
+        return phrase;
+    }
+
+    public void setPhrase(int phrase) {
+        this.phrase = phrase;
+    }
+
+    public RequestHeader getHeader() {
+        return header;
+    }
+
+    public void setHeader(RequestHeader header) {
+        this.header = header;
+    }
 }
