e0ca33d3fe27600d5ebac4fb2d3bc7974d56fb51
William-Andrew Lussier <w_lussie@encs.concordia.ca>
2020-01-31 20:40:45 -0500

    EchoServer + EchoClient for testing environment

--- src/com/httpc/Main.java
+++ src/com/httpc/Main.java
@@ -1,8 +1,31 @@
 package com.httpc;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int server_port = 8080; // Defines at which port the server will be listening at
+        try( ServerSocket server = new ServerSocket(server_port) ){
+            System.out.println("Server has been instantiated at port " + server_port);
+
+            // Server is a process that runs continously and awaits for requests from clients
+            while(true){
+                // Is this a blocking or non-blocking call?
+                // What would you need to do to service multiple clients at the same time?
+                try ( Socket client_connection = server.accept() )
+                {
+                    PrintWriter outbount_client = new PrintWriter(client_connection.getOutputStream(), true);
+                    outbount_client.println("Well hello to you too.");
+                    client_connection.close();
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
