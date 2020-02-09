24aa39147dc3fd4176fad79d022451feb9769ba6
William-Andrew Lussier <w_lussie@encs.concordia.ca>
2020-02-09 18:30:54 -0500

    testing http.sh

---
+++ src/com/httpc/http.sh
@@ -0,0 +1,11 @@
+javac network/exception/InvalidRequestException.java
+javac network/exception/RedirectException.java
+javac network/parameter/HttpBody.java
+javac network/parameter/HttpHeader.java
+javac network/parameter/HttpHeader.java
+javac network/request/GetRequest.java
+javac network/request/PostRequest.java
+javac network/request/Request.java
+javac network/request/RequestHandler.java
+javac network/response/Reponse.java
+javac httpc.java
