6a4910605de553fc54e2451b84e67b87c989d662
William-Andrew Lussier <w_lussie@encs.concordia.ca>
2020-02-09 18:40:39 -0500

    javac not working

---
+++ http.sh
@@ -0,0 +1,15 @@
+javac src/com/httpc/network/exception/InvalidRequestException.java
+javac src/com/httpc/network/exception/RedirectException.java
+
+javac src/com/httpc/network/parameter/HttpBody.java
+javac src/com/httpc/network/parameter/HttpHeader.java
+
+javac src/com/httpc/network/request/Request.java
+javac src/com/httpc/network/request/GetRequest.java
+javac src/com/httpc/network/request/PostRequest.java
+
+javac src/com/httpc/network/response/Reponse.java
+
+javac src/com/httpc/network/request/RequestHandler.java
+
+javac src/com/httpc/httpc.java
